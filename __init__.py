2563b396d0d86c85f227733452d09b4680d4a3ef
Tegegn Wukianos <tegegnwukianost@gmail.com>
2024-10-29 10:48:18 -0700

    configured webhook urls

--- __init__.py
+++ __init__.py
@@ -1,19 +1,40 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 import telebot
 from config import BOT_TOKEN, CHAT_ID
 
 app = Flask(__name__)
 bot = telebot.TeleBot(BOT_TOKEN)
+WEBHOOK_URL = 'https://telebots.alwaysdata.net/t2s/webhook'
 
 @app.route('/')
 def hello_world():
     return "Git is awesome i liked it"
 
-@app.route('/hello')    
-def hello():
-    bot.send_message(CHAT_ID, "Hello, World!")
-    return "message sent!"
+
+
+# Webhook endpoint to handle incoming updates
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    json_data = request.get_json()
+    bot.process_new_updates([telebot.types.Update.de_json(json_data)])
+    return '', 200
+
+
+# Homepage route
+@app.route('/')
+def home():
+    return "Welcome to the TeleBot Flask app!", 200
+
+# Set the webhook
+@app.route('/set_webhook', methods=['GET'])
+def set_webhook():
+    if bot.set_webhook(WEBHOOK_URL):
+        return "Webhook set successfully!", 200
+    else:
+        return "Failed to set webhook.", 400
+    
+
 
 if __name__ == "__main__":
   app.run(debug=True)
