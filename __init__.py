169537bc43510025ca2c0d67a8dd5cc0627a8d03
Tegegn Wukianos <tegegnwukianost@gmail.com>
2024-10-29 10:45:08 -0700

    initialized the bot

--- __init__.py
+++ __init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask import render_template
+import telebot
+from config import BOT_TOKEN, CHAT_ID
 
 app = Flask(__name__)
+bot = telebot.TeleBot(BOT_TOKEN)
 
 @app.route('/')
 def hello_world():
     return "Git is awesome i liked it"
+
+@app.route('/hello')    
+def hello():
+    bot.send_message(CHAT_ID, "Hello, World!")
+    return "message sent!"
 
 if __name__ == "__main__":
   app.run(debug=True)
